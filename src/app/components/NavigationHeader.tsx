fd56d52089594e460a37603ec98ab1095b5f2f68
Troy <devwithtroy@gmail.com>
2025-10-07 12:33:15 -0400

    refactor client component and add header nav

---
+++ src/app/components/NavigationHeader.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+
+interface NavigationHeaderProps {
+  title?: string;
+}
+
+export const NavigationHeader = ({ title }: NavigationHeaderProps) => {
+  return (
+    <div className="h-[60px] w-full bg-[#265b4e] text-[#f5f5f5] flex items-center justify-between px-8">
+      <div className="flex items-center gap-2">
+        <Image src="./wordmark.svg" alt="Solace Logo" width={80} height={60} />
+      </div>
+      <div className="flex items-center gap-2"></div>
+    </div>
+  );
+};
